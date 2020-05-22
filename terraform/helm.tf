945044c6f6492d4d4869a9d98f03ac1907abb3c7
Wojciech Wójcik <wojtaswojcik@gmail.com>
2020-05-22 14:48:35 +0200

    install ambassador edge stack

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,9 +1,10 @@
 resource "helm_release" "metrics-server" {
-  name       = "metrics-server"
-  repository = "https://kubernetes-charts.storage.googleapis.com"
-  chart      = "metrics-server"
-  version    = "2.11.1"
-  namespace  = "kube-system"
+  name        = "metrics-server"
+  repository  = "https://kubernetes-charts.storage.googleapis.com"
+  chart       = "metrics-server"
+  version     = "2.11.1"
+  max_history = 10
+  namespace   = "kube-system"
   set {
     name  = "args[0]"
     value = "--kubelet-insecure-tls"
@@ -16,11 +17,12 @@ resource "helm_release" "metrics-server" {
 }
 
 resource "helm_release" "external-dns" {
-  name       = "external-dns"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "external-dns"
-  version    = "2.20.4"
-  namespace  = "kube-system"
+  name        = "external-dns"
+  repository  = "https://charts.bitnami.com/bitnami"
+  chart       = "external-dns"
+  version     = "2.20.4"
+  max_history = 10
+  namespace   = "kube-system"
   values = [
     "${file("helm-configs/external-dns.yaml")}"
   ]
@@ -32,4 +34,17 @@ resource "helm_release" "external-dns" {
     name  = "cloudflare.email"
     value = var.cloudflare_email
   }
+}
+
+resource "helm_release" "ambassador" {
+  name             = "ambassador"
+  repository       = "https://www.getambassador.io"
+  chart            = "ambassador"
+  max_history      = 10
+  version          = "1.4.3"
+  create_namespace = true
+  namespace        = "ambassador"
+  values = [
+    "${file("helm-configs/ambassador.yaml")}"
+  ]
 }
