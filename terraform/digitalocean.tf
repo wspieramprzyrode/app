7a0f10bf4e57a2ea31a024c5fcdb0e3bdec0c6c1
Wojciech Wójcik <wojtaswojcik@gmail.com>
2020-05-22 08:20:23 +0200

    add flag to helm provide

--- terraform/digitalocean.tf
+++ terraform/digitalocean.tf
@@ -12,14 +12,14 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
   name    = "wspieramprzyrode"
   region  = var.k8s_cluster_region
   version = var.k8s_cluster_version
-  tags = [digitalocean_tag.project_tag.id,digitalocean_tag.cluster_tag.id]
+  tags    = [digitalocean_tag.project_tag.id, digitalocean_tag.cluster_tag.id]
   node_pool {
     name       = "workers"
     size       = var.k8s_cluster_instance_type
     auto_scale = true
     min_nodes  = 1
     max_nodes  = 3
-    tags = [digitalocean_tag.project_tag.id,digitalocean_tag.cluster_tag.id]
+    tags       = [digitalocean_tag.project_tag.id, digitalocean_tag.cluster_tag.id]
   }
 }
 
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,8 +17,9 @@ provider "kubernetes" {
 provider "helm" {
   version = "~> 1.2"
   kubernetes {
-    host  = digitalocean_kubernetes_cluster.cluster.endpoint
-    token = digitalocean_kubernetes_cluster.cluster.kube_config[0].token
+    load_config_file = false
+    host             = digitalocean_kubernetes_cluster.cluster.endpoint
+    token            = digitalocean_kubernetes_cluster.cluster.kube_config[0].token
     cluster_ca_certificate = base64decode(
       digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
     )
